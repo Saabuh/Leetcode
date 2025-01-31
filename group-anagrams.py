0ac614a06f65f822c6976f723eb21e6047d7eafd
saaba <nathan.albino01@gmail.com>
2025-01-31 06:48:56 -0500

    add hashmap only solution

--- group-anagrams.py
+++ group-anagrams.py
@@ -2,36 +2,60 @@
 
 
 class Solution(object):
+
+    # def groupAnagrams(self, strs):
+    #     """
+    #     :type strs: List[str]
+    #     :rtype: List[List[str]]
+    #     hashmap solution using sort
+    #     """
+    #
+    #     # time complexity: O(m * nlogn)
+    #     # space complexity: O(m * n)
+    #
+    #     # create dictionary
+    #     my_dict = {}
+    #     # result array
+    #
+    #     for word in strs:
+    #
+    #         # sorts an interable (string), returns a sorted list of iterable, join() joins the iterable into a single string
+    #         sorted_str = "".join(sorted(word))
+    #
+    #         # check if the sorted word exists as a key in the dictionary
+    #         if sorted_str in my_dict:
+    #
+    #             # append str to list
+    #             my_dict[sorted_str].append(word)
+    #         else:
+    #
+    #             # make the key the sorted str and the value the str
+    #             my_dict[sorted_str] = [word]
+    #
+    #     return list(my_dict.values())
+
     def groupAnagrams(self, strs):
         """
-        :type strs: List[str]
-        :rtype: List[List[str]]
+        hashmap solution using just a hashmap
         """
 
-        # create dictionary
+        # dictionary
         my_dict = {}
-        # result array
-        result = []
 
-        for word in strs:
+        for s in strs:
 
-            # sorts an interable (string), returns a sorted list of iterable, join() joins the iterable into a single string
-            sorted_str = "".join(sorted(word))
+            # character counting array
+            count = [0] * 26
 
-            # check if the sorted word exists as a key in the dictionary
-            if sorted_str in my_dict:
+            for c in s:
+                count[ord(c) - ord("a")] += 1
 
-                # append str to list
-                my_dict[sorted_str].append(word)
+            if tuple(count) in my_dict:
+                my_dict[tuple(count)].append(s)
             else:
+                my_dict[tuple(count)] = [s]
 
-                # make the key the sorted str and the value the str
-                my_dict[sorted_str] = [word]
-
-        for value in my_dict.values():
-            result.append(value)
-
-        return result
+        return list(my_dict.values())
 
 
 solution = Solution()
