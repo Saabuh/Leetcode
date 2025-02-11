c4b4e2c31a8773fa3525a431293c360673ce8d87
saaba <nathan.albino01@gmail.com>
2025-02-11 15:31:34 -0500

    remove encode decode

--- string-encode-and-decode.py
+++ string-encode-and-decode.py
@@ -2,28 +2,3 @@
 # idk
 
 # pylint: disable=R
-
-
-class Solution:
-
-    def encode(self, strs: list[str]) -> str:
-
-        encoded_str = " ".join(strs)
-
-        return encoded_str
-
-    def decode(self, s: str) -> list[str]:
-        """decodes a string after being encoded with encode() function"""
-
-        decoded_str = s.split(" ")
-
-        return decoded_str
-
-
-solution = Solution()
-# print(f"encoded string: {solution.encode(["neet", "code", "love", "you"])}")
-print(f"encoded string: {solution.encode([""])}")
-
-# encoded_string = solution.encode(["neet", "code", "love", "you"])
-encoded_string = solution.encode([""])
-print(f"decoded string: {solution.decode(encoded_string)}")
